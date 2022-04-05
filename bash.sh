9e2ad38da26745265fbbe6f145d2ba4890130ff1
Lukasz Pitrus <plgpitrus@login01.prometheus.cyfronet.pl>
2022-04-05 13:42:05 +0200

    generator

--- bash.sh
+++ bash.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
+#SBATCH --mem=1K
 #SBATCH --time=00:05:00
 #SBATCH --partition=plgrid-short
 #SBATCH --account=plgyaptide
 
 module add plgrid/tools/openmp
 
-g++ -Wall static/main_stolen.cpp -o static/main_stolen -fopenmp
-./static/main_stolen
+gcc -Wall static/generate.c -o static/generate -fopenmp
+./static/generate
 
 # for (( i=12; i>0; i-- ))
 # do
--- static/generate.c
+++ static/generate.c
@@ -1,27 +1,33 @@
 #include <omp.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
     int SIZE_OF_TABLE = 10000;
     int AMOUNT_OF_THREADS = 4;
     int *array;
+    int MAX_NUMBER = 10000;
     array = malloc(SIZE_OF_TABLE * sizeof(int));
     double t0 = omp_get_wtime();
-#pragma omp parallel num_threads(AMOUNT_OF_THREADS)
+    #pragma omp parallel num_threads(AMOUNT_OF_THREADS)
     {
-#pragma omp single
+        int i;
+        unsigned int seed = 2137;
+        #pragma omp for schedule(static, SIZE_OF_TABLE / AMOUNT_OF_THREADS) private(i)
+        for (i = 0; i < SIZE_OF_TABLE; i++)
         {
-            printf(" Liczba wątków: %d \n", omp_get_num_threads());
-            printf(" Jestem wątek nr %d \n ", omp_get_thread_num());
-            printf("\n");
-        }
-#pragma omp for schedule(static, SIZE_OF_TABLE / AMOUNT_OF_THREADS)
-        for (int i = 0; i < SIZE_OF_TABLE; i++)
-        {
-            array[i] = omp_get_thread_num();
+            array[i] = rand_r(&seed)%MAX_NUMBER;
         }
     }
     double t1 = omp_get_wtime();
     printf("\n");
     printf(" Czas wykonania: %f \n", t1 - t0);
+
+    int i;
+    for (i = 0; i<SIZE_OF_TABLE; i++) {
+        printf("%d \n", array[i]);
+    }
+
+    return 0;
 }
