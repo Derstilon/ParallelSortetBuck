d357074e9934c4d7c45d2900a203e7561d900d57
Lukasz Pitrus <plgpitrus@login02.prometheus.cyfronet.pl>
2022-04-07 14:19:48 +0200

    nth

--- src/bucketStort2.c
+++ src/bucketStort2.c
@@ -9,6 +9,8 @@ int min(int a, int b) {
 
 void bucketSort2(int number_of_threads, int array_size, int *array, int *sorted_array, int bucket_size, int number_of_buckets, int bucket_range)
 {
+    sorted_array = malloc(array_size * sizeof(int))
+
     int BASIC_CHUNK_SIZE = array_size / number_of_threads;
     int *chunks_sizes = malloc(number_of_threads * sizeof(int));
     int i, j, x;
