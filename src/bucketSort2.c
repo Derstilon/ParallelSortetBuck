aeba0946e66d9071895aa8d912b55079d659a492
Lukasz Pitrus <plgpitrus@login02.prometheus.cyfronet.pl>
2022-04-07 14:39:40 +0200

    bash

--- src/bucketSort2.c
+++ src/bucketSort2.c
@@ -2,14 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
 void bucketSort2(int number_of_threads, int array_size, int *array, int *sorted_array, int bucket_size, int number_of_buckets, int bucket_range)
 {
-    sorted_array = malloc(array_size * sizeof(int))
+    sorted_array = malloc(array_size * sizeof(int));
 
     int BASIC_CHUNK_SIZE = array_size / number_of_threads;
     int *chunks_sizes = malloc(number_of_threads * sizeof(int));
@@ -52,6 +49,6 @@ void bucketSort2(int number_of_threads, int array_size, int *array, int *sorted_
         {
             printf(" %d ", buckets[i][j]);
         }
-        printf("/n");
+        printf("\n");
     }
 }
