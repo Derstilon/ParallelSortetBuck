e65767887c0e44ce9be47f753518ca53d2c69a41
Karolk99 <konkolisz@gmail.com>
2022-04-11 22:44:55 +0200

    Add time measurment for bucketSort1

--- src/bucketSort1.c
+++ src/bucketSort1.c
@@ -8,7 +8,7 @@ void bucketSort1(int number_of_threads, int array_size, int *array, int *sorted_
 {
     sorted_array = malloc(number_of_buckets * bucket_size * sizeof(int));
     int i = 0, j = 0, k = 0;
-    double t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0, t7 = 0;
+    double t0 = 0, t1 = 0, t2 = 0, t3 = 0;
     int buckets_per_thread = number_of_buckets / number_of_threads;
 
     int **bucket_fullness = malloc(number_of_buckets * sizeof(int *));
@@ -28,8 +28,8 @@ void bucketSort1(int number_of_threads, int array_size, int *array, int *sorted_
 
 #pragma omp parallel private(i) num_threads(number_of_threads)
     {
+        t0 = omp_get_wtime();
         int thread_no = omp_get_thread_num();
-
         // place in correct bucket
         for (i = 0; i < array_size; i++)
         {
@@ -41,13 +41,15 @@ void bucketSort1(int number_of_threads, int array_size, int *array, int *sorted_
                 bucket_length[thread_no * number_of_buckets / number_of_threads + bucket_no]++;
             }
         }
-
+        t1 = omp_get_wtime();
         // sort buckets
         for (i = thread_no * number_of_buckets / number_of_threads; i < (thread_no + 1) * number_of_buckets / number_of_threads; i++)
         {
             sortChunk(&sorted_array[i * bucket_size], bucket_length[i]);
         }
+        t2 = omp_get_wtime();
     }
+
     int index = 0;
     for (i = 0; i < number_of_buckets; i++)
     {
@@ -56,10 +58,14 @@ void bucketSort1(int number_of_threads, int array_size, int *array, int *sorted_
             array[index++] = sorted_array[j];
         }
     }
-    for (i = 0; i < array_size; i++)
-    {
-        printf("%d ", array[i]);
-    }
+    t3 = omp_get_wtime();
+    //     for (i = 0; i < array_size; i++)
+    //     {
+    //         printf("%d ", array[i]);
+    //     }
+    printf("Czas czas podziału na kubełki: %e \n", t1 - t0);
+    printf("Czas sortowania kubełków: %e \n", t2 - t1);
+    printf("Czas sortowania całej tablicy: %e \n", t3 - t0);
 }
 
 int main()
